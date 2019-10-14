5f9ff6e14c9f2a5472f7278dc5dfe387912e51b8
D7703_16 <d7703_16@d7703-16ui-imac.local>
2019-10-14 16:37:42 +0900

    3:)

--- SimpleGeocoding02/ViewController.swift
+++ SimpleGeocoding02/ViewController.swift
@@ -14,10 +14,39 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let addr = "부산광역시 부산진구 양지로 54"
+        //let addr = "부산광역시 부산진구 양지로 54"
+        let lat = 35.16587040
+        let lng = 129.07284610
+        let loc = CLLocation(latitude: lat, longitude: lng)
         var pins = [MKPointAnnotation]()
         let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(loc, completionHandler: {
+            (placemarks:[CLPlacemark]?, error : Error?) in
+            if error != nil{
+            print(error!)
+            }
+            if let myPlacemarks = placemarks {
+                let myPlacemark = myPlacemarks.first
+                print(myPlacemark?.country)
+                print(myPlacemark?.administrativeArea)
+                print(myPlacemark?.locality)
+                print(myPlacemark?.name)
+                //핀 꼽기
+                let pin = MKPointAnnotation()
+                pin.coordinate = (myPlacemark?.location!.coordinate)!
+                pin.title = "동의과학대학교"
+                pin.subtitle = "우리들의 꿈이 자라는 곳!"
+                pins.append(pin)
+                self.mapView.showAnnotations(pins, animated: true)
+                } else {
+                    print("nil 발생")
+                }
+        })
+        }
+        
+        
+        
         //인자 2개 받음. 2번째거는 함수로 받음.
 //        geocoder.geocodeAddressString(addr, completionHandler: {
 //            (placemarks:[CLPlacemark]?, error : Error?) in
@@ -43,37 +72,37 @@ class ViewController: UIViewController {
 //            }
 //
 //        })
-        
-        //후행 클로저(trailing closure
-        //geocoder.geocodeAddressString(addr) {
-        //    (placemarks:[CLPlacemark]?, error : Error?) in
-        //    if error != nil{
-        //        print(error!)
-        //    }
-        geocoder.geocodeAddressString(addr) {
-            placemarks, error in
-            if error != nil{
-                print(error!)
-            }
-            // optional binding check
-            if let myPlacemarks = placemarks {
-                let myPlacemark = myPlacemarks.first
-                print(myPlacemark?.country)
-                print(myPlacemark?.location?.coordinate)
-                
-                //핀 꼽기
-                let pin = MKPointAnnotation()
-                pin.coordinate = (myPlacemark?.location!.coordinate)!
-                pin.title = "동의과학대학교"
-                pin.subtitle = "우리들의 꿈이 자라는 곳!"
-                pins.append(pin)
-                self.mapView.showAnnotations(pins, animated: true)
-                
-            } else {
-                print("nil 발생")
-            }
-            
-        }
+//
+//        //후행 클로저(trailing closure
+//        //geocoder.geocodeAddressString(addr) {
+//        //    (placemarks:[CLPlacemark]?, error : Error?) in
+//        //    if error != nil{
+//        //        print(error!)
+//        //    }
+//        geocoder.geocodeAddressString(addr) {
+//            placemarks, error in
+//            if error != nil{
+//                print(error!)
+//            }
+//            // optional binding check
+//           if let myPlacemarks = placemarks {
+//                let myPlacemark = myPlacemarks.first
+//                print(myPlacemark?.country)
+//                print(myPlacemark?.location?.coordinate)
+//
+//                //핀 꼽기
+//                let pin = MKPointAnnotation()
+//                pin.coordinate = (myPlacemark?.location!.coordinate)!
+//                pin.title = "동의과학대학교"
+//                pin.subtitle = "우리들의 꿈이 자라는 곳!"
+//                pins.append(pin)
+//                self.mapView.showAnnotations(pins, animated: true)
+//
+//            } else {
+//               print("nil 발생")
+//            }
+//
+//        }
     }
-}
+
 
